37bd67f94581fb545dc2fbfa17b2b0a156881def
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2015-01-21 23:47:22 +0000

    Initial commit

---
+++ src/LaunchAnagram.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Pierre on 21/01/2015.
+ */
+public class LaunchAnagram {
+    public static void main(String[] args) {
+        LaunchAnagram l = new LaunchAnagram();
+        l.run();
+    }
+
+    public void run() {
+        
+    }
+}
